9b0d94875b0a8e5eadf09d1796518a18aaa31173
Brad Pizzimenti <brad@gennakersystems.com>
2016-02-26 14:37:22 -0800

    WIP first test of clear database, cannot find sql2o package

---
+++ src/main/java/Stylist.java
@@ -0,0 +1,19 @@
+// import org.sql2o.*;
+import java.util.List;
+
+public class Stylist {
+  private int id;
+  private String name;
+
+  public Stylist (String name) {
+    this.name = name;
+  }
+
+  //read all stylists from database
+  public static List<Stylist> all() {
+    try (Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM stylists";
+      return con.createQuery(sql).exectuteAndFetch(Stylist.class);
+    }
+  }
+}
